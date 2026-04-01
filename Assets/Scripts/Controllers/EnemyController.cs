876be8696c7fb5d7b451599f970f958323cdd529
Bamidele Ogundeji <developers@previouslypluto.com>
2026-04-01 16:21:27 -0400

    Refactor: replace enemyList with enemyData in EnemyController; add new DialogueManager and EncounterManager scripts

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class EnemyController : MonoBehaviour
 {
-    public List<FighterSO> enemyList;
+    [SerializeField] private FighterSO enemyData;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
