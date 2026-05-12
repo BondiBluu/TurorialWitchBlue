543dac2d4f5d937a7667df5fc8b2b27ebba155ee
Bamidele Ogundeji <developers@previouslypluto.com>
2026-05-12 15:06:10 -0400

    Refactor MoveManager to enhance move info display; add hover functionality and remove unused elements

--- Assets/Scripts/Battle/MoveManager.cs
+++ Assets/Scripts/Battle/MoveManager.cs
@@ -10,7 +10,6 @@ public class MoveManager : MonoBehaviour
     [SerializeField] GameObject magicDescPanel;
     [SerializeField] TMP_Text moveName;
     [SerializeField] TMP_Text movePwr;
-    [SerializeField] TMP_Text moveElement;
     [SerializeField] TMP_Text moveDesc;
     [SerializeField] TMP_Text moveMP;
     [SerializeField] GameObject moveButtonPrefab;
@@ -20,7 +19,6 @@ public class MoveManager : MonoBehaviour
     
     void Start()
     {
-        HideMoveInfo();
         battlePlayerManager = FindAnyObjectByType<BattlePlayerManager>();
         fightingButtonsManager = FindAnyObjectByType<FightingButtonsManager>();
     }
@@ -38,16 +36,17 @@ public class MoveManager : MonoBehaviour
                 moveButton.GetComponentInChildren<TMP_Text>().text = move.MoveName;
 
                 //add hover logic
+                MoveSO currentMove = move;
                 EventTrigger trigger = moveButton.AddComponent<EventTrigger>();
 
                 EventTrigger.Entry entry = new EventTrigger.Entry();
                 entry.eventID = EventTriggerType.PointerEnter;
-                entry.callback.AddListener(_ => ShowMoveInfo());
+                entry.callback.AddListener(_ => ShowMoveInfo(currentMove));
                 trigger.triggers.Add(entry);
 
                 EventTrigger.Entry exit = new EventTrigger.Entry();
                 exit.eventID = EventTriggerType.PointerExit;
-                exit.callback.AddListener(_ => HideMoveInfo());
+                exit.callback.AddListener(_ => fightingButtonsManager.ClosePanel(magicDescPanel));
                 trigger.triggers.Add(exit);
             }
         }
@@ -61,12 +60,13 @@ public class MoveManager : MonoBehaviour
         }
     }
 
-    public void ShowMoveInfo()
+    public void ShowMoveInfo(MoveSO move)
     {
         magicDescPanel.SetActive(true);
-    }
-    public void HideMoveInfo()
-    {
-        magicDescPanel.SetActive(false);
+
+        moveName.text = move.MoveName;
+        movePwr.text = $"PWR: {move.AttackAmount}";
+        moveMP.text = $"{move.MagicPointsNeeded}";
+        moveDesc.text = move.MoveDesc;
     }
 }
