cc66079a939fd797133b88e74e4d3dba430d496e
Bamidele Ogundeji <developers@previouslypluto.com>
2026-06-03 15:20:27 -0400

    Refactor action management by removing Action class and implementing BattleAction in SaveAction

--- Assets/Scripts/Battle/BattleAction.cs
+++ Assets/Scripts/Battle/BattleAction.cs
@@ -1,10 +1,10 @@
-public class Action
+public class BattleAction
     {
         public MoveSO move;
         public FighterSO user;
         public FighterSO target;
 
-        public Action(FighterSO _user, MoveSO _move, FighterSO _target)
+        public BattleAction(FighterSO _user, MoveSO _move, FighterSO _target)
         {
             user = _user;
             move = _move;
--- Assets/Scripts/Battle/SaveAction.cs
+++ Assets/Scripts/Battle/SaveAction.cs
@@ -3,5 +3,33 @@ using UnityEngine;
 
 public class SaveAction : MonoBehaviour
 {
-    List<Action> actionQueue = new List<Action>();
+    List<BattleAction> actionQueue = new List<BattleAction>();
+    public FighterSO pendingUser;
+    public MoveSO pendingMove;
+    public FighterSO pendingTarget;
+
+    public void SetUser(FighterSO user)
+    {
+        pendingUser = user;
+    }
+
+    public void SetMove(MoveSO move)
+    {
+        pendingMove = move;
+    }
+
+    public void SetTarget(FighterSO target)
+    {
+        pendingTarget = target;
+    }
+
+    //add to list of attacks and set all to null for the next turn
+    public void AddToQueue()
+    {
+        actionQueue.Add(new BattleAction(pendingUser, pendingMove, pendingTarget));
+
+        pendingUser = null;
+        pendingMove = null;
+        pendingTarget = null;
+    }
 }
